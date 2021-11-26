2c2bbc52041e70875f0f4732cce0c237f8e60c9f
Marcus <marcusclemmons1@gmail.com>
2021-11-26 15:20:16 -0500

    last 30 done

--- PDSE.py
+++ PDSE.py
@@ -74,7 +74,7 @@ def reverse_string(phrase):
         >>> reverse_string('sauce')
         'ecuas'
     """
-return phrase[: : -1]
+    return phrase[: : -1]
 #6
 def single_letter_count(word, letter):
     """How many times does letter appear in word (case-insensitively)?
@@ -152,8 +152,8 @@ def list_manipulation(lst, command, location, value=None):
         >>> list_manipulation(lst, 'add', 'dunno') is None
         True
     """
-#had to look at solution
-if command == "remove":
+    #had to look at solution
+    if command == "remove":
         if location == "end":
             return lst.pop()
         elif location == "beginning":
@@ -166,7 +166,6 @@ if command == "remove":
         elif location == "end":
             lst.append(value)
             return lst
-
 #9
 def is_palindrome(phrase):
     """Is phrase a palindrome?
@@ -209,7 +208,692 @@ def frequency(lst, search_term):
     """
     return lst.count(search_term)
 
+#11
+def flip_case(phrase, to_swap):
+    """Flip [to_swap] case each time it appears in phrase.
+
+        >>> flip_case('Aaaahhh', 'a')
+        'aAAAhhh'
+
+        >>> flip_case('Aaaahhh', 'A')
+        'aAAAhhh'
+
+        >>> flip_case('Aaaahhh', 'h')
+        'AaaaHHH'
+
+    """
+    #had to look at solution
+    to_swap = to_swap.lower()
+    out = ""
+
+    for ltr in phrase:
+        if ltr.lower() == to_swap:
+            ltr = ltr.swapcase()
+        out += ltr
+
+    return out
+#12
+def multiply_even_numbers(nums):
+    """Multiply the even numbers.
     
+        >>> multiply_even_numbers([2, 3, 4, 5, 6])
+        48
+        
+        >>> multiply_even_numbers([3, 4, 5])
+        4
+        
+    If there are no even numbers, return 1.
+    
+        >>> multiply_even_numbers([1, 3, 5])
+        1
+    """
+    total = 1
+    for num in nums:
+        if num % 2 == 0:
+             total = total * num
+
+    return total
+#13
+def capitalize(phrase):
+    """Capitalize first letter of first word of phrase.
+
+        >>> capitalize('python')
+        'Python'
+
+        >>> capitalize('only first word')
+        'Only first word'
+    """
+    return phrase.capitalize()
+#14
+def compact(lst):
+    """Return a copy of lst with non-true elements removed.
+
+        >>> compact([0, 1, 2, '', [], False, (), None, 'All done'])
+        [1, 2, 'All done']
+    """
+    
+    new_list = []
+    for item in list:
+        if item == False:
+            return removed(item)
+    return new_list.append(item)
+#15
+def intersection(l1, l2):
+    """Return intersection of two lists as a new list::
+    
+        >>> intersection([1, 2, 3], [2, 3, 4])
+        [2, 3]
+        
+        >>> intersection([1, 2, 3], [1, 2, 3, 4])
+        [1, 2, 3]
+        
+        >>> intersection([1, 2, 3], [3, 4])
+        [3]
+        
+        >>> intersection([1, 2, 3], [4, 5, 6])
+        []
+    """
+    return list(set(l1 & l2))
+#16
+def partition(lst, fn):
+    """Partition lst by predicate.
+     
+     - lst: list of items
+     - fn: function that returns True or False
+     
+     Returns new list: [a, b], where `a` are items that passed fn test,
+     and `b` are items that failed fn test.
+
+        >>> def is_even(num):
+        ...     return num % 2 == 0
+        
+        >>> def is_string(el):
+        ...     return isinstance(el, str)
+        
+        >>> partition([1, 2, 3, 4], is_even)
+        [[2, 4], [1, 3]]
+        
+        >>> partition(["hi", None, 6, "bye"], is_string)
+        [['hi', 'bye'], [None, 6]]
+    """
+    return [ [val for val in lst if fn(val)], [val for val in lst if not fn(val)] ]
+#17
+def mode(nums):
+    """Return most-common number in list.
+
+    For this function, there will always be a single-most-common value;
+    you do not need to worry about handling cases where more than one item
+    occurs the same number of times.
+
+        >>> mode([1, 2, 1])
+        1
+
+        >>> mode([2, 2, 3, 3, 2])
+        2
+    """
+    #had to look at solution
+    counts = {}
+
+    for num in nums:
+        counts[num] = counts.get(num, 0) + 1
+    max_value = max(counts.values())
+    for (num, freq) in counts.items():
+        if freq == max_value:
+            return num
+
+#18
+def calculate(operation, a, b, make_int=False, message='The result is'):
+    """Perform operation on a + b, ()possibly truncating) & returning w/msg.
+
+    - operation: 'add', 'subtract', 'multiply', or 'divide'
+    - a and b: values to operate on
+    - make_int: (optional, defaults to False) if True, truncates to integer
+    - message: (optional) message to use (if not provided, use 'The result is')
+
+    Performs math operation (truncating if make_int), then returns as
+    "[message] [result]"
+
+        >>> calculate('add', 2.5, 4)
+        'The result is 6.5'
+
+        >>> calculate('subtract', 4, 1.5, make_int=True)
+        'The result is 2'
+
+        >>> calculate('multiply', 1.5, 2)
+        'The result is 3.0'
+
+        >>> calculate('divide', 10, 4, message='I got')
+        'I got 2.5'
+
+    If a valid operation isn't provided, return None.
+
+        >>> calculate('foo', 2, 3)
+        
+    """
+    # had to look at solution
+    if operation == "add":
+        res = a + b
+    elif operation == "subtract":
+        res = a - b
+    elif operation == "multiply":
+        res = a * b
+    elif operation == "divide":
+        res = a / b
+    else:
+        return
+
+    if make_int:
+        res = int(res)
+
+    return f"{message} {res
+
+#19
+def friend_date(a, b):
+    """Given two friends, do they have any hobbies in common?
+
+    - a: friend #1, a tuple of (name, age, list-of-hobbies)
+    - b: same, for friend #2
+
+    Returns True if they have any hobbies in common, False is not.
+
+        >>> elmo = ('Elmo', 5, ['hugging', 'being nice'])
+        >>> sauron = ('Sauron', 5000, ['killing hobbits', 'chess'])
+        >>> gandalf = ('Gandalf', 10000, ['waving wands', 'chess'])
+
+        >>> friend_date(elmo, sauron)
+        False
+
+        >>> friend_date(sauron, gandalf)
+        True
+    """
+    if set(a[2]) & set(b[2]):
+        return True
+    else:
+        return False
+#20
+def triple_and_filter(nums):
+    """Return new list of tripled nums for those nums divisible by 4.
+
+    Return every number in list that is divisible by 4 in a new list,
+    except multipled by 3.
+    
+        >>> triple_and_filter([1, 2, 3, 4])
+        [12]
+        
+        >>> triple_and_filter([6, 8, 10, 12])
+        [24, 36]
+        
+        >>> triple_and_filter([1, 2])
+        []
+    """
+    return [num * 3 for num in nums if num % 4 == 0]
+
+
+#21
+def extract_full_names(people):
+    """Return list of names, extracting from first+last keys in people dicts.
+
+    - people: list of dictionaries, each with 'first' and 'last' keys for
+              first and last names
+
+    Returns list of space-separated first and last names.
+
+        >>> names = [
+        ...     {'first': 'Ada', 'last': 'Lovelace'},
+        ...     {'first': 'Grace', 'last': 'Hopper'},
+        ... ]
+
+        >>> extract_full_names(names)
+        ['Ada Lovelace', 'Grace Hopper']
+    """
+    return [f"{person['first']} {person['last']}" for person in people]
+
+#22
+def sum_floats(nums):
+    """Return sum of floating point numbers in nums.
+    
+        >>> sum_floats([1.5, 2.4, 'awesome', [], 1])
+        3.9
+        
+        >>> sum_floats([1, 2, 3])
+        0
+    """
+    #had to look at solution
+    return sum([num for num in nums if isinstance(num, float)])
+
+
+    # hint: to find out if something is a float, you should use the
+    # "isinstance" function --- research how to use this to find out
+    # if something is a float!
+
+#23
+def list_check(lst):
+    """Are all items in lst a list?
+
+        >>> list_check([[1], [2, 3]])
+        True
+
+        >>> list_check([[1], "nope"])
+        False
+    """
+    for item in lst:
+        if not isinstance(item, list):
+            return False
+        else:
+            True
+#24
+def remove_every_other(lst):
+    """Return a new list of other item.
+
+        >>> lst = [1, 2, 3, 4, 5]
+
+        >>> remove_every_other(lst)
+        [1, 3, 5]
+
+    This should return a list, not mutate the original:
+
+        >>> lst
+        [1, 2, 3, 4, 5]
+    """
+
+    return lst[::2]
+
+#25
+def sum_pairs(nums, goal):
+    """Return tuple of first pair of nums that sum to goal.
+
+    For example:
+
+        >>> sum_pairs([1, 2, 2, 10], 4)
+        (2, 2)
+
+    (4, 2) sum to 6, and come before (5, 1):
+
+        >>> sum_pairs([4, 2, 10, 5, 1], 6) # (4, 2)
+        (4, 2)
+
+    (4, 3) sum to 7, and finish before (5, 2):
+
+        >>> sum_pairs([5, 1, 4, 8, 3, 2], 7)
+        (4, 3)
+
+    No pairs sum to 100, so return empty tuple:
+
+        >>> sum_pairs([11, 20, 4, 2, 1, 5], 100)
+        ()
+    """
+    #had to look at solution
+    already_visited = set()
+
+    for i in nums:
+        difference = goal - i
+
+        if difference in already_visited:
+            return (difference, i)
+
+        already_visited.add(i)
+
+    return ()
+
+#26
+def vowel_count(phrase):
+    """Return frequency map of vowels, case-insensitive.
+
+        >>> vowel_count('rithm school')
+        {'i': 1, 'o': 2}
+        
+        >>> vowel_count('HOW ARE YOU? i am great!') 
+        {'o': 2, 'a': 3, 'e': 2, 'u': 1, 'i': 1}
+    """
+    #had to look at solution
+    phrase = phrase.lower()
+    counter = {}
+
+    for ltr in phrase:
+        if ltr in VOWELS:
+            counter[ltr] = counter.get(ltr, 0) + 1
+
+    return counter
+
+#27
+def titleize(phrase):
+    """Return phrase in title case (each word capitalized).
+
+        >>> titleize('this is awesome')
+        'This Is Awesome'
+
+        >>> titleize('oNLy cAPITALIZe fIRSt')
+        'Only Capitalize First'
+    """
+    return phrase.title()
+
+#28
+def find_factors(num):
+    """Find factors of num, in increasing order.
+
+    >>> find_factors(10)
+    [1, 2, 5, 10]
+
+    >>> find_factors(11)
+    [1, 11]
+
+    >>> find_factors(111)
+    [1, 3, 37, 111]
+
+    >>> find_factors(321421)
+    [1, 293, 1097, 321421]
+    """
+    #had to look at the solution
+    factors = [x for x in range (1, num // 2 + 1) if num % x == 0]
+
+    factors.append(num)
+
+    return factors
+
+#29
+def includes(collection, sought, start=None):
+    """Is sought in collection, starting at index start?
+
+    Return True/False if sought is in the given collection:
+    - lists/strings/sets/tuples: returns True/False if sought present
+    - dictionaries: return True/False if *value* of sought in dictionary
+
+    If string/list/tuple and `start` is provided, starts searching only at that
+    index. This `start` is ignored for sets/dictionaries, since they aren't
+    ordered.
+
+        >>> includes([1, 2, 3], 1)
+        True
+
+        >>> includes([1, 2, 3], 1, 2)
+        False
+
+        >>> includes("hello", "o")
+        True
+
+        >>> includes(('Elmo', 5, 'red'), 'red', 1)
+        True
+
+        >>> includes({1, 2, 3}, 1)
+        True
+
+        >>> includes({1, 2, 3}, 1, 3)  # "start" ignored for sets!
+        True
+
+        >>> includes({"apple": "red", "berry": "blue"}, "blue")
+        True
+    """
+    #had to look at solution
+    if isinstance(collection, dict):
+        return sought in collection.values()
+
+    if start is None or isinstance(collection, set):
+        return sought in collection
+
+    return sought in collection[start:]
+
+#30
+def repeat(phrase, num):
+    """Return phrase, repeated num times.
+
+        >>> repeat('*', 3)
+        '***'
+
+        >>> repeat('abc', 2)
+        'abcabc'
+
+        >>> repeat('abc', 0)
+        ''
+
+    Ignore illegal values of num and return None:
+
+        >>> repeat('abc', -1) is None
+        True
+
+        >>> repeat('abc', 'nope') is None
+        True
+    """
+    if not isinstance(num, int) or num < 0:
+        return None
+    else:
+        return phrase * num
+#31
+def truncate(phrase, n):
+    """Return truncated-at-n-chars version of  phrase.
+    
+    If the phrase is longer than, or the same size as, n make sure it ends with '...' and is no
+    longer than n.
+    
+        >>> truncate("Hello World", 6)
+        'Hel...'
+        
+        >>> truncate("Problem solving is the best!", 10)
+        'Problem...'
+        
+        >>> truncate("Yo", 100)
+        'Yo'
+        
+    The smallest legal value of n is 3; if less, return a message:
+    
+        >>> truncate('Cool', 1)
+        'Truncation must be at least 3 characters.'
+
+        >>> truncate("Woah", 4)
+        'W...'
+
+        >>> truncate("Woah", 3)
+        '...'
+    """
+    #had to look at solution
+    if n < 3:
+        return "Truncation must be at least 3 characters."
+
+    if n > len(phrase) + 2:
+        return phrase
+
+    return phrase[:n - 3] + "..."
+#32
+def two_list_dictionary(keys, values):
+    """Given keys and values, make dictionary of those.
+    
+        >>> two_list_dictionary(['x', 'y', 'z'], [9, 8, 7])
+        {'x': 9, 'y': 8, 'z': 7}
+        
+    If there are fewer values than keys, remaining keys should have value
+    of None:
+    
+        >>> two_list_dictionary(['a', 'b', 'c', 'd'], [1, 2, 3])
+        {'a': 1, 'b': 2, 'c': 3, 'd': None}
+    
+    If there are fewer keys, ignore remaining values:
+
+        >>> two_list_dictionary(['a', 'b', 'c'], [1, 2, 3, 4])
+        {'a': 1, 'b': 2, 'c': 3}
+   """
+   #had to look at solution
+   out = {}
+
+    for idx, val in enumerate(keys):
+        out[val] = values[idx] if idx < len(values) else None
+
+    return out
+#33
+def sum_range(nums, start=0, end=None):
+    """Return sum of numbers from start...end.
+
+    - start: where to start (if not provided, start at list start)
+    - end: where to stop (include this index) (if not provided, go through end)
+
+        >>> nums = [1, 2, 3, 4]
+
+        >>> sum_range(nums)
+        10
+
+        >>> sum_range(nums, 1)
+        9
+
+        >>> sum_range(nums, end=2)
+        6
+
+        >>> sum_range(nums, 1, 3)
+        9
+
+    If end is after end of list, just go to end of list:
+
+        >>> sum_range(nums, 1, 99)
+        9
+    """
+    if end is None:
+        end = len(nums)
+
+    return sum(nums[start:end + 1])
+
+#34
+def same_frequency(num1, num2):
+    """Do these nums have same frequencies of digits?
+    
+        >>> same_frequency(551122, 221515)
+        True
+        
+        >>> same_frequency(321142, 3212215)
+        False
+        
+        >>> same_frequency(1212, 2211)
+        True
+    """
+    #had to look at solution
+    return freq_counter(str(num1)) == freq_counter(str(num2))
+#35
+def two_oldest_ages(ages):
+    """Return two distinct oldest ages as tuple (second-oldest, oldest)..
+
+        >>> two_oldest_ages([1, 2, 10, 8])
+        (8, 10)
+
+        >>> two_oldest_ages([6, 1, 9, 10, 4])
+        (9, 10)
+
+    Even if more than one person has the same oldest age, this should return
+    two *distinct* oldest ages:
+
+        >>> two_oldest_ages([1, 5, 5, 2])
+        (2, 5)
+    """
+    #had to look at solution
+    uniq_ages = set(ages)
+    oldest = sorted(uniq_ages)[-2:]
+    return tuple(oldest)
+
+    # NOTE: don't worry about an optimized runtime here; it's fine if
+    # you have a runtime worse than O(n)
+
+    # NOTE: you can sort lists with lst.sort(), which works in place (mutates);
+    # you may find it helpful to research the `sorted(iter)` function, which
+    # can take *any* type of list-like-thing, and returns a new, sorted list
+    # from it.
+
+#36
+def find_the_duplicate(nums):
+    """Find duplicate number in nums.
+
+    Given a list of nums with, at most, one duplicate, return the duplicate.
+    If there is no duplicate, return None
+
+        >>> find_the_duplicate([1, 2, 1, 4, 3, 12])
+        1
+
+        >>> find_the_duplicate([6, 1, 9, 5, 3, 4, 9])
+        9
+
+        >>> find_the_duplicate([2, 1, 3, 4]) is None
+        True
+    """
+    #had to look at solution
+    
+    seen = set()
+
+    for num in nums:
+        if num in seen:
+            return num
+        seen.add(num)
+
+#37
+def sum_up_diagonals(matrix):
+    """Given a matrix [square list of lists], return sum of diagonals.
+
+    Sum of TL-to-BR diagonal along with BL-to-TR diagonal:
+
+        >>> m1 = [
+        ...     [1,   2],
+        ...     [30, 40],
+        ... ]
+        >>> sum_up_diagonals(m1)
+        73
+
+        >>> m2 = [
+        ...    [1, 2, 3],
+        ...    [4, 5, 6],
+        ...    [7, 8, 9],
+        ... ]
+        >>> sum_up_diagonals(m2)
+        30
+    """
+    #had to look at solution
+    total = 0
+
+    for i in range(len(matrix)):
+        total += matrix[i][i]
+        total += matrix[i][-1 - i]
+
+    return total
+#38
+def min_max_keys(d):
+    """Return tuple (min-keys, max-keys) in d.
+
+        >>> min_max_keys({2: 'a', 7: 'b', 1: 'c', 10: 'd', 4: 'e'})
+        (1, 10)
+
+    Works with any kind of key that can be compared, like strings:
+
+        >>> min_max_keys({"apple": "red", "cherry": "red", "berry": "blue"})
+        ('apple', 'cherry')
+    """
+    tool = d.keys()
+
+    return (min(tool), max(tool))
+
+#39
+def find_greater_numbers(nums):
+    """Return # of times a number is followed by a greater number.
+
+    For example, for [1, 2, 3], the answer is 3:
+    - the 1 is followed by the 2 *and* the 3
+    - the 2 is followed by the 3
+
+    Examples:
+
+        >>> find_greater_numbers([1, 2, 3])
+        3
+
+        >>> find_greater_numbers([6, 1, 2, 7])
+        4
+
+        >>> find_greater_numbers([5, 4, 3, 2, 1])
+        0
+
+        >>> find_greater_numbers([])
+        0
+    """
+    #had to look at solution
+    count = 0
+
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)):
+            if nums[j] > nums[i]:
+                count += 1
+
+    return count
+
 
 
 
